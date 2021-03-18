0f033a34e44c9452476368b7ef6ea34aa668b9e3
Asherah Connor <ashe@kivikakk.ee>
2021-03-18 23:14:01 +0000

    poll: CSS classes changed

--- poll.rb
+++ poll.rb
@@ -8,7 +8,7 @@ out = `curl -s -H"Cookie: #{COOKIE}" #{URL}`
 if out =~ /class="no-sub"/
   system "printf \"furaffinity.login\" | nc -NU ../lilac/lilac.sock"
 else
-  count = out.scan(/notelinknote-unread/).count
+  count = out.scan(/note-unread/).count
   if count > 0
     system "printf \"furaffinity.#{count}\" | nc -NU ../lilac/lilac.sock"
   end
