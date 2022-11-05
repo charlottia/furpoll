b6f7882ab7dd0667ab47acf4e971d0050522ed64
Asherah Connor <asherah@hrzn.ee>
2022-11-05 02:09:39 +0000

    update mail.sh for sendmail

--- mail.sh
+++ mail.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0")" || exit 1
 	echo "Subject: furpoll"
 	echo 'Content-Type: text/html'
 	echo 'From: Asherah Connor <furpoll@asherah.hrzn.ee>'
+	echo 'To: Asherah Connor <asherah@hrzn.ee>'
 	echo ''
 	echo "$@"
-) | /usr/sbin/ssmtp asherah@hrzn.ee
+) | /usr/sbin/sendmail -t
